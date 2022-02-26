16791fa79cda8dc5552dbeccb376da06aeadce60
beucismis <beucismis@tutamail.com>
2022-02-27 00:42:01 +0300

    Update subscriber.py

--- easynewsletter/subscriber.py
+++ easynewsletter/subscriber.py
@@ -1,21 +1,25 @@
+import time
 import util
 
 
 class Subscriber:
-    def __init__(self, email: str, status: bool = 1):
+    def __init__(self, email: str, active: bool = True):
         self.email = email
-        self.status = status
+        self.active = active
         self.id = util.generate_id(email)
-        self.created = None
+        self.created = time.strftime("%Y-%m-%d %H:%M:%S")
 
         if not util.is_email(email):
             raise util.InvalidMail()
 
     def __repr__(self) -> str:
-        return f"<Subscriber(email={self.email}, status={self.status}, id={self.id})>"
+        return f"<Subscriber(email={self.email}, active={self.active}, id={self.id}), created={self.created}>"
 
     def user_name(self) -> str:
         return self.email.split("@")[0]
 
     def domain(self) -> str:
         return self.email.split("@")[1]
+
+    def to_tuple(self) -> tuple:
+        return (self.email, self.active, self.id, self.created)
